44b3b601a2eda93cbae482a3a8d443517ab2aad5
peter michael <enitanpeters28@gmail.com>
2023-01-24 11:06:52 +0100

    ok

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,4 +1,4 @@
-const whitelist = [ 'http://localhost:3000', 'https://inventoryapp-eight.vercel.app/', 'http://127.0.0.1:4500', 'https://www.google.com/', "https://inventoryapp-eight.vercel.app/", 'https://www.google.com.ar' ];
+const whitelist = [ 'https://inventory-frontend-xi.vercel.app/', 'http://127.0.0.1:4500', 'https://www.google.com/', "https://inventoryapp-eight.vercel.app/", 'https://www.google.com.ar' ];
 
 module.exports = whitelist;
 
