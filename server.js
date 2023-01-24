e958445928f6834f86d876ed6d9550fd0e3b118c
peter michael <enitanpeters28@gmail.com>
2023-01-24 10:32:24 +0100

    ok

--- server.js
+++ server.js
@@ -27,10 +27,11 @@ app.use( express.json() ); //to handle our json
 app.use( cookieParser() ); // cookie parser
 app.use( express.urlencoded( { extended: false } )); //handle data via url
 app.use( bodyParser.json() ); // to parse the data sent in the body from the frontend
-app.use( cors({
-    origin: ["http://localhost:3000", 'https://inventoryapp-eight.vercel.app/'],
-    credentials: true,
-  }) ); // cors
+app.use( cors() ); // cors
+// cors({
+//     origin: ["http://localhost:3000", 'https://inventoryapp-eight.vercel.app/'],
+//     credentials: true,
+//   })
 
 //setting the upload routes
 app.use('/uploads', express.static(path.join(__dirname, "uploads")))
